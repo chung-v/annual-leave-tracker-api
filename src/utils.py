9a96dcd55796e24ff9b9ec70b4328033daae865d
chung-v <vcdott@gmail.com>
2024-09-28 20:48:51 +1000

    added admin decorator function.

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,25 @@
 from flask_jwt_extended import get_jwt_identity
 
+from init import db
+from models.employee import Employee
+
 import functools
 
-from init import db
-from models.user import User
 
-#  Checks if the current user is an admin before allowing the decorated function to execute.
+#  Checks if the current employee is an admin before allowing the decorated function to execute.
 def auth_as_admin_decorator(fn):
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
-        # Gets the user's id from get_jwt_identity.
-        user_id = get_jwt_identity()
-        # Fetches the user from the database.
-        stmt = db.select(User).filter_by(id=user_id)
-        user = db.session.scalar(stmt)
-        # If the user is an admin, the function is executed.
-        if user.is_admin:
+        # Gets the employee's id from get_jwt_identity.
+        employee_id = get_jwt_identity()
+        # Fetches the employee from the database.
+        stmt = db.select(Employee).filter_by(id=employee_id)
+        employee = db.session.scalar(stmt)
+        # If the employee is an admin, the function is executed.
+        if employee.is_admin:
             return fn(*args, **kwargs)
         # If not, error 403 is returned.
         else:
-            return {"error": "Only admin can perform this action"}, 403
+            return {"error": "Only an admin can perform this action."}, 403
     
     return wrapper
